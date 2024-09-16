ce0e1684aa48de0631fb04cc748a35393c65e109
Dimitry Sibulskis <sibulskis.dimitry@hotmail.com>
2024-09-16 18:47:39 -0300

    cadastro banco de dados

--- Cadastro/index.js
+++ Cadastro/index.js
@@ -1,15 +1,24 @@
 function validateFields(){
-  const email = document.getElementById("email").value
-  if (!email){
-    document.getElementById('recover-button').disabled = true;
-  }
-  else if (validateEmail(email)){
-    document.getElementById('recover-button').disabled = false;
-  }
-  else{
-    document.getElementById('recover-button').disabled = true;
-  }
+  const emailValid = isEmailValid();
+  document.getElementById('recover-button').disabled = !emailValid;
 
+  const password = isPasswordValid();
+  document.getElementById('login button').disable = !emailValid || !passwordValid;
+}
+
+function isEmailValid() {
+ const email = document.getElementById("email").value
+ if (!email) {
+    return false;
+ }
+ return validateEmail(email);
+}
+function isPasswordValid() {
+  const password = document.getElementById("password").value;
+  if (!password) {
+      return false;
+  }
+  return true;
 }
 
 function validateEmail(email) {
