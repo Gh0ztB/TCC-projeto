a81932af4b756ad113aaf1b03becb364dff6533f
Dimitry Sibulskis <sibulskis.dimitry@hotmai.com>
2024-09-18 22:02:23 -0300

    cadastro

--- Cadastro/index.js
+++ Cadastro/index.js
@@ -1,26 +1,38 @@
 function validateFields(){
-  const emailValid = isEmailValid();
-  document.getElementById('recover-button').disabled = !emailValid;
-
-  const password = isPasswordValid();
-  document.getElementById('login button').disable = !emailValid || !passwordValid;
+    toggleButtonsDisable();
+    toggleEmailErrors();
 }
 
 function isEmailValid() {
- const email = document.getElementById("email").value
- if (!email) {
-    return false;
- }
- return validateEmail(email);
+    const email = document.getElementById("email").value
+    if (!email) {
+        return false;
+    }
+    return validateEmail(email);
+}
+function toggleEmailErrors(){
+    const email = document.getElementById('email').value;
+    if (!email){
+        document.getElementById('email-required-error').style.display = block;
+    }
+}
+function toggleButtonsDisable(){
+    const emailValid = isEmailValid();
+    document.getElementById('recover-button').disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById('login-button').disabled = !passwordValid || !emailValid;
+
 }
 function isPasswordValid() {
-  const password = document.getElementById("password").value;
-  if (!password) {
-      return false;
-  }
-  return true;
+    const password = document.getElementById('password').value;
+    if (!password) {
+        return false;
+    }
+     return true;
+
 }
 
 function validateEmail(email) {
-    return /\S+0\S+\.S+/.test(email);
+    return /\S+0\S+\.\S+/.test(email);
 }
